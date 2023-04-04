a84f78b098efed3ca38a40f6d12c5cddfedc777b
Germán Escobar <german.escobarc@gmail.com>
2023-04-04 18:05:14 -0500

    Modify docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://guides.makeitreal.camp/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -19,10 +19,12 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'makeitrealcamp', // Usually your GitHub org/user name.
-  projectName: 'guia', // Usually your repo name.
+  projectName: 'new-guides', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -42,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/khriztianmoreno/mir-guide/tree/main/',
+            'https://github.com/makeitrealcamp/new-guides/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/khriztianmoreno/mir-guide/tree/main/',
+            'https://github.com/makeitrealcamp/new-guides/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -68,7 +70,7 @@ const config = {
       navbar: {
         title: 'Make It Real',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Make It Real Logo',
           src: 'img/logo.jpg',
         },
         items: [
